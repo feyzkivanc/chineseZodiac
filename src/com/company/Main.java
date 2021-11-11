436f9fb67f39c9825c5f80a2d79be070f2120051
feyzkivanc <feyzakivanc@gmail.com>
2021-11-11 20:34:09 +0300

    Çin Burcu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,54 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Doğum yılınızı giriniz: ");
+        int year= scan.nextInt();
+        int number= year%12;
+        switch(number){
+            case 0:
+                System.out.println("Çin zodyağınız: " + "Maymun");
+                break;
+            case 1:
+                System.out.println("Çin zodyağınız: " + "Horoz");
+                break;
+            case 2:
+                System.out.println("Çin zodyağınız: " + "Köpek");
+                break;
+            case 3:
+                System.out.println("Çin zodyağınız: " + "Domuz");
+                break;
+            case 4:
+                System.out.println("Çin zodyağınız: " + "Fare");
+                break;
+            case 5:
+                System.out.println("Çin zodyağınız: " + "Öküz");
+                break;
+            case 6:
+                System.out.println("Çin zodyağınız: " + "Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin zodyağınız: " + "Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin zodyağınız: " + "Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin zodyağınız: " + "Yılan");
+                break;
+            case 10:
+                System.out.println("Çin zodyağınız: " + "At");
+                break;
+            case 11:
+                System.out.println("Çin zodyağınız: " + "Koyun");
+                break;
+            default:
+                break;
+        }
     }
 }
